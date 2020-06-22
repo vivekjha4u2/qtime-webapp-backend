007a1feda91e9758f9016b17706847911b7d2f91
unknown <vivekjha4u2@gmail.com>
2020-06-22 15:13:37 +0530

    get req for answers

--- src/main/java/com/infy/api/QuizReplyAPI.java
+++ src/main/java/com/infy/api/QuizReplyAPI.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value="/quizreply")
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuizReplyAPI {
     @Autowired
     private QuizReplyService quizReplyService;
